fc4e42f2a2b77b8bb026042c9ce40d660b61677f
Richard Kalehoff <richardkalehoff@gmail.com>
2015-11-16 15:44:53 -0500

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name udaciMealsAngularApp
+ * @description
+ * # udaciMealsAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('udaciMealsAngularApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udaciMealsAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the udaciMealsAngularApp
+ */
+angular.module('udaciMealsAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
